712df37e6e6f816fdea3870b217b4fcb191f4426
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-04-12 10:29:42 -0500

    funcionando

---
+++ FestivalMusica-inicio/gulpfile.js
@@ -0,0 +1,77 @@
+const { src, dest, watch, parallel } = require('gulp');
+
+// CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+// const autoprefixer = require('autoprefixer');
+// const cssnano = require('cssnano');
+// const postcss = require('gulp-postcss');
+// const sourcemaps = require('gulp-sourcemaps');
+
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+// Javascript
+// const terser = require('gulp-terser-js');
+
+function css( done ) {
+    src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( dest('dist/css') ) // Almacenarla en el disco duro
+    done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('dist/img') )
+    done();
+}
+
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('dist/img') )
+    done();
+}
+
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('dist/img') )
+    done();
+}
+
+
+function dev( done ) {
+    watch('src/scss/**/*.scss', css);
+    // watch('src/js/**/*.js', javascript);
+    done();
+}
+
+function tarea (done) {
+    console.log('Desde la primera tarea');
+    done();
+}
+ 
+exports.tarea = tarea;
+
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+// exports.dev = dev;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev) ;
